cd04dc0c0000af9ccc5658cb579a8b0217954874
ivanhuay <ivan.huayrana@despegar.com>
2016-05-30 08:57:30 -0300

    git files ignored during copy process

--- install.py
+++ install.py
@@ -12,17 +12,24 @@ def copyrecursively(source_folder, destination_folder):
         for item in files:
             src_path = os.path.join(root, item)
             dst_path = os.path.join(destination_folder, src_path.replace(source_folder, ""))
-            if os.path.exists(dst_path):
-                if os.stat(src_path).st_mtime > os.stat(dst_path).st_mtime:
+            if(".git" not in src_path):
+                if os.path.exists(dst_path):
+                    if os.stat(src_path).st_mtime > os.stat(dst_path).st_mtime:
+                        shutil.copy2(src_path, dst_path)
+                else:
                     shutil.copy2(src_path, dst_path)
             else:
-                shutil.copy2(src_path, dst_path)
+                print "ignore file: "+src_path
+
         for item in dirs:
+
             src_path = os.path.join(root, item)
-            dst_path = os.path.join(
-                destination_folder, src_path.replace(source_folder, ""))
-            if not os.path.exists(dst_path):
-                os.mkdir(dst_path)
+            dst_path = os.path.join(destination_folder, src_path.replace(source_folder, ""))
+            if(".git" not in src_path):
+                if not os.path.exists(dst_path):
+                    os.mkdir(dst_path)
+            else:
+                print "ignore folder: "+src_path
 
 gokachu_path = config.read("HOME").replace("$HOME", _HOME)
 print "gokachu path:" + gokachu_path
